ebb68708abf67fd2ad5e6cb71702ec7847cd99d1
jjtheshooterr <jaxon.thomas03@gmail.com>
2025-12-02 00:01:36 -0600

    jh

--- src/components/home/ProcessStepsAnimated.tsx
+++ src/components/home/ProcessStepsAnimated.tsx
@@ -49,10 +49,6 @@ export default function MethodologyInfinityStripWithHeader() {
         </motion.p>
       </div>
 
-      {/* Fading edges */}
-      <div className="pointer-events-none absolute inset-y-0 left-0 w-24 bg-gradient-to-r from-background to-transparent z-10" />
-      <div className="pointer-events-none absolute inset-y-0 right-0 w-24 bg-gradient-to-l from-background to-transparent z-10" />
-
       <Strip />
     </section>
   );
